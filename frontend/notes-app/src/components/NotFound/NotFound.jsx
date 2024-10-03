e55f34ff2e565d667ee9f3d8f235d539b064687c
AfsalRoxix <mohamedafsal237@gmail.com>
2024-10-03 09:02:30 +0530

    first commit

---
+++ frontend/notes-app/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='text-center text-stone-700 mt-7 font-semibold text-2xl'> 404 | NotFound</div>
+  )
+}
+
+export default NotFound
